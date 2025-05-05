f81fe43909a8133eb484273bdd3b098e483de1ac
M-Unity <mihailsheshukov10@gmail.com>
2025-05-05 17:25:47 +0500

    AAAA

--- Assets/_Client/Scripts/PlayerController.cs
+++ Assets/_Client/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float Speed = 10;
     [SerializeField] private float JumpForce = 2;
+    [SerializeField] private Collider2D Collider;
+    [SerializeField] private LayerMask Ground;
     private float VelocityX;
     private Vector2 Movement;
-    private bool IsGrounded;
     private Rigidbody2D RigidBody;
+    private bool CanJump=true;
     private void Awake()=>RigidBody=GetComponent<Rigidbody2D>();
     private void Update()
     {
@@ -15,19 +17,17 @@ public class PlayerController : MonoBehaviour
         VelocityX=Mathf.Lerp(VelocityX,Movement.x*Speed,10*Time.deltaTime);
         RigidBody.linearVelocityX=VelocityX;
         RigidBody.rotation=Movement.x.Equals(1)?90:-90;
-        if(InputHandler.Jump.IsPressed()&&IsGrounded)Jump();
+        if(InputHandler.Jump.WasPressedThisFrame()&&Collider.IsTouchingLayers(Ground)&&CanJump)Jump();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void UpdateFriction()
     {
-        IsGrounded = true;
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        IsGrounded = false;
+        
     }
     private void Jump()
     {   
-        IsGrounded = false;
+        CanJump=false;
         RigidBody.linearVelocity += JumpForce * Vector2.up;
+        Invoke(nameof(MayIJump),0.25f);
     }
+    private void MayIJump()=>CanJump=true;
 }
